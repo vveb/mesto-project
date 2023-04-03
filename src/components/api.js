25c74cded6db3c8949b2f6a3ec57d7d25e2e2a7d
Victor Tarapata <vvteach@yandex.ru>
2023-04-03 22:30:32 +0300

    fix: reduce checkResponse to fetch-function

--- src/components/api.js
+++ src/components/api.js
@@ -19,42 +19,35 @@ function goFetch({ endpoint, data, method}) {
     options.method = method;
     options.body = JSON.stringify(data);
   }
-  return fetch(endpoint, options);
+  return fetch(endpoint, options).then(checkResponseOk);
 }
 
 function getProfileData() {
   return goFetch({ endpoint: endpointURLs.profile, method: 'GET' })
-  .then(checkResponseOk);
 }
 
 function loadCards() {
   return goFetch({ endpoint: endpointURLs.cards, method: 'GET' })
-  .then(checkResponseOk);
 }
 
 function addNewCard(data) {
   return goFetch({ endpoint: endpointURLs.cards, data, method: 'POST' })
-  .then(checkResponseOk);
 }
 
 function deleteCardData(cardId) {
   return goFetch({ endpoint: `${endpointURLs.cards}/${cardId}`, method: 'DELETE' })
-    .then(checkResponseOk);
 }
 
 function editProfileInfo(data) {
   return goFetch({ endpoint: endpointURLs.profile, data, method: 'PATCH' })
-    .then(checkResponseOk);
 }
 
 function editProfileAvatar(data) {
   return goFetch({ endpoint: endpointURLs.avatar, data, method: 'PATCH' })
-    .then(checkResponseOk);
 }
 
 function toggleLike({ cardId, isLiked }) {
   return goFetch({ endpoint: `${endpointURLs.likes}/${cardId}`, method: isLiked ? 'DELETE' : 'PUT' })
-    .then(checkResponseOk);
 }
 
 export const api = {
